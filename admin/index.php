ba96b15b8b76d872a63816d9aa07c31a1e9af10b
duongmien <duongthimien7@gmail.com>
2021-09-19 06:18:45 +0700

    Gui admin control

---
+++ admin/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/admin.css">
+  <title>Food Order Website - Home Page</title>
+</head>
+
+<body>
+  <div class="menu text-center">
+    <div class="wrapper">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Admin</a></li>
+        <li><a href="">Category</a></li>
+        <li><a href="">Food</a></li>
+        <li><a href="">Order</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="main-content">
+    <div class="wrapper">
+      <h1>Dashboard</h1>
+      <div class="col-4 text-center">
+        <h2>5</h2>
+        <br />
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h2>5</h2>
+        <br />
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h2>5</h2>
+        <br />
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h2>5</h2>
+        <br />
+        Category
+      </div>
+      <div class="col-4 text-center">
+        <h2>5</h2>
+        <br />
+        Category
+      </div>
+      <div class="clearfix"></div>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="wrapper">
+      <p class="text-center">All rights reserved. Designed By <a href="#">Dribbe</a></p>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/admin.css">
+  <title>Food Order Website - Home Page</title>
+</head>
+
+<body>
+  <div class="menu text-center">
+    <div class="wrapper">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Admin</a></li>
+        <li><a href="">Category</a></li>
+        <li><a href="">Food</a></li>
+        <li><a href="">Order</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="main-content">
+    <div class="wrapper">
+      <h1>Manage Admin</h1>
+    </div>
+  </div>
+  <div class="footer">
+    <div class="wrapper">
+      <p class="text-center">All rights reserved. Designed By <a href="#">Dribbe</a></p>
+    </div>
+  </div>
+</body>
+
+</html>
