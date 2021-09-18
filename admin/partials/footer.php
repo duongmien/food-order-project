b7eef4771bc303efa0f4b848b655b407ec380b24
duongmien <duongthimien7@gmail.com>
2021-09-19 06:25:48 +0700

    update gui admin control

---
+++ admin/partials/footer.php
@@ -0,0 +1,8 @@
+<div class="footer">
+    <div class="wrapper">
+      <p class="text-center">All rights reserved. Designed By <a href="#">Dribbe</a></p>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/reset.css">
+  <link rel="stylesheet" href="../css/admin.css">
+  <title>Food Order Website - Home Page</title>
+</head>
+
+<body>
+  <div class="menu text-center">
+    <div class="wrapper">
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="manage-admin.php">Admin</a></li>
+        <li><a href="">Category</a></li>
+        <li><a href="">Food</a></li>
+        <li><a href="">Order</a></li>
+      </ul>
+    </div>
+  </div>
