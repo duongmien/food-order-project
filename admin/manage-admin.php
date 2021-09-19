e6875ff079cd303ac897ab380a6e1692a062de22
duongmien <duongthimien7@gmail.com>
2021-09-19 07:38:49 +0700

    update Gui admin control

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,7 +1,55 @@
 <?php include('./partials/menu.php') ?>
-  <div class="main-content">
-    <div class="wrapper">
-      <h1>Manage Admin</h1>
-    </div>
+<div class="main-content">
+  <div class="wrapper">
+    <h1>Manage Admin</h1>
+    <br />
+    <a href="" class="btn-primary">Add Admin</a>
+    <br /><br /><br /><br />
+    <table class="tbl-full">
+      <tr>
+        <th>ID</th>
+        <th>Full Name</th>
+        <th>Username</th>
+        <th>Actions</th>
+      </tr>
+      <tr>
+        <td>1.</td>
+        <td>Duong Thi Mien</td>
+        <td>duongmien</td>
+        <td>
+          <a class="btn-secondary" href="">Update Admin</a>
+          <a class="btn-danger" href=""> Delete Admin</a>
+        </td>
+      </tr>
+      <tr>
+        <td>2.</td>
+        <td>Duong Thi Mien</td>
+        <td>duongmien</td>
+        <td>
+          <a class="btn-secondary" href="">Update Admin</a>
+          <a class="btn-danger" href=""> Delete Admin</a>
+        </td>
+      </tr>
+      <tr>
+        <td>3.</td>
+        <td>Duong Thi Mien</td>
+        <td>duongmien</td>
+        <td>
+          <a class="btn-secondary" href="">Update Admin</a>
+          <a class="btn-danger" href=""> Delete Admin</a>
+        </td>
+      </tr>
+      <tr>
+        <td>4.</td>
+        <td>Duong Thi Mien</td>
+        <td>duongmien</td>
+        <td>
+          <a class="btn-secondary" href="">Update Admin</a>
+          <a class="btn-danger" href=""> Delete Admin</a>
+        </td>
+      </tr>
+
+    </table>
   </div>
-  <?php include('./partials/footer.php') ?>
+</div>
+<?php include('./partials/footer.php') ?>
