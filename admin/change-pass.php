0f8a11ad87281018be70a8d3d1b716a071db03d1
duongmien <duongthimien7@gmail.com>
2021-09-22 13:59:48 +0700

    change password

--- admin/change-pass.php
+++ admin/change-pass.php
@@ -4,65 +4,80 @@
     <br>
     <h1>Change Password</h1>
     <br><br>
+
     <?php
-    if (isset($_GET['id'])) {
-      $id = $_GET['id'];
-    }
+      if(isset($_GET['id'])){
+        $id=$_GET['id'];
+      }
     ?>
+
     <form action="" method="POST">
       <table class="tbl-30">
         <tr>
           <td>Current Password:</td>
-          <td><input type="password" name="ma_pass" placeholder="Current Password"></td>
+          <td>
+            <input type="password" name="current_pass" placeholder="Current Password">
+          </td>
         </tr>
         <tr>
           <td>New Password:</td>
-          <td><input type="password" name="new_password" placeholder="New Password"></td>
+          <td>
+            <input type="password" name="new_pass" placeholder="New Password">
+          </td>
         </tr>
         <tr>
           <td>Confirm Password:</td>
-          <td><input type="password" name="confirm_password" placeholder="Confirm Password"></td>
+          <td>
+            <input type="password" name="confirm_pass" placeholder="Confirm Password">
+          </td>
         </tr>
         <tr>
           <td colspan="2">
             <input type="hidden" name="id" value="<?php echo $id; ?>">
-            <input type="submit" name="submit" value="Change Password" class="btn-secondary">
+            <input type="submit" name="submit" value="Change Password">
           </td>
         </tr>
       </table>
     </form>
   </div>
 </div>
+
 <?php
-//check submit button clik
-if (isset($_POST['submit'])) {
-  // Get data from form
-  $id = $_POST['id'];
-  $current_password = md5($_POST['ma_pass']);
-  $new_pass = md5($_POST['new_password']);
-  $confirm_pass = md5($_POST['confirm_password']);
-}
-//SQL
-$sql = " SELECT * FROM tbl_admin WHERE id=$id AND password='$current_password'";
-//Excute
-$res = mysqli_query($conn, $sql);
-if ($res == TRUE) {
-  $count = mysqli_num_rows($res);
-  if ($count == 1) {
-    if ($new_pass == $confirm_pass) {
-      $sql2 = "UPDATE tbl_admin SET password='$new_pass' WHERE id=$id";
-      $res2 = mysqli_query($conn, $sql2);
-      if ($res2 == TRUE) {
-        $_SESSION['change-pass'] = "Password Changed Successfully";
-      } else {
-        $_SESSION['pass-not-match'] = "Password Did not Match. Please try again!";
+    //Check if submit button is click
+    if(isset($_POST['submit'])){
+      // 1.Get the data from Form
+      $id=$_POST['id'];
+      $current_pass=md5($_POST['current_pass']);
+      $new_pas= md5($_POST['new_pass']);
+      $confirm_pass=md5($_POST['confirm_pass']);
+      // 2. Check user with current ID and Current Password exist or not
+      $sql= "SELECT * FROM tbl_admin WHERE id=$id AND password='$current_pass'";
+      // Excute sql
+      $res= mysqli_query($conn,$sql);
+      if($res==true){
+        // check data is available or not
+        $count= mysqli_num_rows($res);
+        if($count==1)
+        {
+          //User Exists and Password can be change
+          // echo "User found";
+          // Check the new pas and confirm pass match or not
+          if($new_pas==$confirm_pass){
+            // Update Pass
+            echo "Pass match";
+          }
+          else{
+            $_SESSION['pass-not-match']="<div class='text-error'>Password Did not Match</div>";
+            header('location:'.SITEURL.'admin/manage-admin.php');
+          }
+        }
+        else{
+          $_SESSION['user-not-found']="<div class='text-error'>User Not Found</div>";
+          header('location:'.SITEURL.'admin/manage-admin.php');
+        }
       }
-    } else {
-      $_SESSION['pass-not-match'] = "Password Did not Match. Please try again!";
+      // 3. Check New Pass and Confirm Pass match or not
+      // 4. Change Pass if all abobe is true
     }
-  } else {
-    $_SESSION['user-not-found'] = "User Not Found";
-  }
-}
 ?>
 <?php include('./partials/footer.php') ?>
