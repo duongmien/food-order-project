320200f6b5c0eafcdefb620fdbaf69d5c89473c0
duongmien <duongthimien7@gmail.com>
2021-09-26 17:10:42 +0700

    update category session

---
+++ admin/update-category.php
@@ -0,0 +1,70 @@
+<?php include('./partials/menu.php') ?>
+<div class="main-content">
+  <div class="wrapper">
+    <h1>Update Category</h1>
+    <br><br><br>
+    <!-- Seclect data from Home Category -->
+    <?php
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM tbl_category WHERE id= $id";
+    $res = mysqli_query($conn, $sql);
+    if ($res == TRUE) {
+      $count = mysqli_num_rows($res);
+      if ($count == 1) {
+        $row = mysqli_fetch_assoc($res);
+        $title = $row['title'];
+        $active = $row['active'];
+        $current_image = $row['image_name'];
+        $featured = $row['featured'];
+      } else {
+        header("location:" . SITEURL . 'admin/manage-category.php');
+      }
+    }
+    ?>
+    <!-- Add category Form -->
+    <form action="" method="POST" enctype="multipart/form-data">
+      <table class="tbl-30">
+        <tr>
+          <td>New Title:</td>
+          <td>
+            <input type="text" name="title" value="<?php echo $title ?>">
+          </td>
+        </tr>
+        <tr>
+          <td>Current Image: </td>
+          <td>
+            <?php
+            if ($current_image != "") {
+              //  Display the Image
+            ?>
+              <img src="<?php echo SITEURL; ?>images/category/<?php echo $current_image ?>" width="150px">
+            <?php
+            } else {
+              // Display Message
+              echo "<div class='text-error'> Image not Added. </div>";
+            }
+            ?>
+          </td>
+        </tr>
+        <tr>
+          <td>Featured: </td>
+          <td>
+            <input type="radio" name="featured" value="Yes"> Yes
+            <input type="radio" name="featured" value="No"> No
+          </td>
+        </tr>
+        <tr>
+          <td>Active: </td>
+          <td>
+            <input type="radio" name="active" value="Yes">Yes
+            <input type="radio" name="active" value="No">No
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <input type="submit" name="submit" value="Add Category" class="btn-secondary">
+          </td>
+        </tr>
+      </table>
+    </form>
+    <!-- Form end -->
